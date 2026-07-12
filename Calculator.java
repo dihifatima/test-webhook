25a7bcd466f9286577d7640ded988dfd981aa6da
dihifatima <dihifatima08@gmail.com>
2026-07-12 18:32:48 +0100

    ajout Calculator.java pour test SonarCloud

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Sum: " + calc.add(5, 3));
+        System.out.println("Difference: " + calc.subtract(5, 3));
+    }
+}
